17dd57d90c50aec07458d3a303813ba5d720fded
Kleber Silva <kb-max@hotmail.com>
2021-12-10 22:09:13 -0300

    Initial commit

---
+++ src/main/java/com/myapp/springbootapppractice/SpringBootAppPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.springbootapppractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAppPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAppPracticeApplication.class, args);
+	}
+
+}
